befb9976ce94ab30beb1fac70061473ca59ed51f
Hubbard Harvey <hubbard.harvey@gmail.com>
2022-07-16 19:59:55 -0400

    Install Tailwind, build out structure

---
+++ src/routes/header.svelte
@@ -0,0 +1,5 @@
+<div class="flex grid-rows-3 justify-around font-serif">
+    <div class="text-4xl">H3</div>
+    <div class="text-4xl">Salvation</div>
+    <div class="text-4xl">Projects</div>
+</div>
