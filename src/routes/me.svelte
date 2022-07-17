a7c34e9b118a5444fefd7397e40ad20cb806a70c
Hubbard Harvey <hubbard.harvey@gmail.com>
2022-07-16 22:17:00 -0400

    styling the portfolio section

---
+++ src/routes/me.svelte
@@ -0,0 +1,5 @@
+<div class="grid grid-rows-3 gap-10 h-full">
+  <h4 class="text-left">Who I am</h4>
+  <h4 class="text-center">How I can help</h4>
+  <h4 class="text-right">Love to help Christian Ministries</h4>
+</div>
--- src/routes/portfolio.svelte
+++ src/routes/portfolio.svelte
@@ -1,1 +1,20 @@
-<h1>Portfolio</h1>
+<div class="flex flex-col">
+  <h1 class="text-center">My Work</h1>
+  <div class="grid grid-rows-2 grid-flow-col gap-4">
+    <div>
+      Box
+    </div>
+    <div>
+      Box
+    </div>
+    <div>
+      Box
+    </div>
+    <div>
+      Box
+    </div>
+    <div>
+      Box
+    </div>
+  </div>
+</div>
